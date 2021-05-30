0e42e42508624c7832056e98a91a7a92d713ec24
Rahul <rahulrao0209@gmail.com>
2021-05-30 15:22:40 +0530

    updated reviews section and added buttons

--- src/components/reviews-section/Review.js
+++ src/components/reviews-section/Review.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { InlineIcon } from '@iconify/react';
+import previousIcon from '@iconify/icons-grommet-icons/previous';
+import nextIcon from '@iconify/icons-grommet-icons/next';
 import './review.css';
 
 export const Review = () => {
@@ -14,11 +17,18 @@ export const Review = () => {
                     </p>
                 </div>
                 <div className="review-section-wrapper-reviews">
-                    <img
-                        className="review-image" 
-                        src="./assets/images/review-section-images/customer1.png"
-                        alt="reviewer-customer"
-                    />
+                    <div>
+                        <InlineIcon icon={previousIcon} className="review-button previous" />
+                        <img
+                            className="review-image" 
+                            src="./assets/images/review-section-images/customer1.png"
+                            alt="reviewer-customer"
+                        />
+                        <InlineIcon icon={nextIcon} className="review-button next"/>
+                    </div>
+                    <p className="reviewer-name">
+                        Rita Sweet
+                    </p>
                     <p className="review-comment">
                      Superb app! The rates are live and real time and the charting is very good.
                      Supports major Indian markets along with FnO too!
