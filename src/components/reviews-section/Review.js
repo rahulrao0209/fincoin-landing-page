5b83fa96ff7506804b6935b550b56cce8042e9d6
Rahul <rahulrao0209@gmail.com>
2021-06-05 13:46:21 +0530

    added customer reviews

--- src/components/reviews-section/Review.js
+++ src/components/reviews-section/Review.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { InlineIcon } from '@iconify/react';
 import previousIcon from '@iconify/icons-grommet-icons/previous';
 import nextIcon from '@iconify/icons-grommet-icons/next';
+import customers from './customers.json';
 import './review.css';
 
 export const Review = () => {
+    const [index, setIndex] = React.useState(1);
     return (
         <section className="review-section">
             <div className="review-section-wrapper">
@@ -18,20 +20,23 @@ export const Review = () => {
                 </div>
                 <div className="review-section-wrapper-reviews">
                     <div>
-                        <InlineIcon icon={previousIcon} className="review-button previous" />
+                        <InlineIcon icon={previousIcon} className="review-button previous"
+                            onClick={() => setIndex(index === 0 ? customers.length - 1 : index - 1)}
+                        />
                         <img
                             className="review-image" 
-                            src="./assets/images/review-section-images/customer1.png"
+                            src={customers[index].image}
                             alt="reviewer-customer"
                         />
-                        <InlineIcon icon={nextIcon} className="review-button next"/>
+                        <InlineIcon icon={nextIcon} className="review-button next"
+                            onClick={() => setIndex(index === customers.length - 1 ? 0: index + 1)}
+                        />
                     </div>
                     <p className="reviewer-name">
-                        Rita Sweet
+                       {customers[index].name}
                     </p>
                     <p className="review-comment">
-                     Superb app! The rates are live and real time and the charting is very good.
-                     Supports major Indian markets along with FnO too!
+                     {customers[index].review}
                     </p>
                 </div>
             </div>
